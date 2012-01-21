26837d74b6b42299b2597041f80ec6b7d4ebc48a
David Estes <destes@redwindsw.com>
2012-01-21 15:48:41 -0500

    added javascript functions for nested attribute forms

--- app/helpers/spud/admin/application_helper.rb
+++ app/helpers/spud/admin/application_helper.rb
@@ -3,4 +3,15 @@ module Spud::Admin::ApplicationHelper
 			return "Never" if timedate.blank?
 			return Time.now() - timedate > 604800 ? timedate.strftime("%B %d") + ' at ' + timedate.strftime("%I:%M %p") : time_ago_in_words(timedate) + ' ago'
 	end
+    def link_to_remove_fields(name, f)
+	    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)")
+    end
+  
+  def link_to_add_fields(name, f, association)
+    new_object = f.object.class.reflect_on_association(association).klass.new
+    fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
+      render(association.to_s.singularize + "_fields", :f => builder)
+    end
+    link_to_function(name, "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")")
+  end
 end
