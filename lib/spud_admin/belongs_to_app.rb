9929c116e8f1ce5b5fc84c042a6d1c405deebc5c
David Estes <destes@redwindsw.com>
2012-01-25 16:23:30 -0500

    improved belongs_to_spud_app command

--- lib/spud_admin/belongs_to_app.rb
+++ lib/spud_admin/belongs_to_app.rb
@@ -9,12 +9,22 @@ module Spud::BelongsToApp
 				Spud::Core.admin_applications.each do |app|
 					if app[:name].parameterize.underscore.to_sym == name
 						@@page_thumbnail=app[:thumbnail]
+						@@page_application = app
 					end
 				end
-				before_filter :instantize_global_vars
-				def instantize_global_vars
+				
+				before_filter { |controller|
 					@page_thumbnail = @@page_thumbnail
-				end	
+					@page_name = @@page_application[:name]
+					if controller.action_name == 'new' || controller.action_name == 'create'
+						@page_name = "New #{@page_name.singularize}"
+					elsif controller.action_name == 'edit' || controller.action_name == 'update'
+							@page_name = "Edit #{@page_name.singularize}"
+					elsif controller.action_name == 'show'
+						@page_name = "Show #{@page_name.singularize}"
+					end
+				}
+					
 			end
 
 		end
