dfef3757b716e68694317f7ad30b8d4a314a46e7
David Estes <destes@redwindsw.com>
2012-01-27 19:31:34 -0500

    updated configuration options

--- lib/spud_admin/configuration.rb
+++ lib/spud_admin/configuration.rb
@@ -1,12 +1,8 @@
 module Spud
   module Core
     include ActiveSupport::Configurable
-
-    config_accessor :site_name,:admin_applications,:s3_credentials,:paperclip_storage,:sitemap_urls
-
+    config_accessor :site_name,:admin_applications,:sitemap_urls
     self.admin_applications = [{:name => "Users",:thumbnail => "spud/admin/users_thumb.png",:url => "/spud/admin/users",:order => 100}]
-    self.paperclip_storage = :filesystem
-    self.s3_credentials = "#{Rails.root}/config/s3.yml"
     self.site_name = "Company Name"
     self.sitemap_urls = []
   end
