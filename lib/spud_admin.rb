144f24ee97194fd3f971e3e27d37b054c6f51662
David Estes <destes@redwindsw.com>
2012-01-29 22:47:49 -0500

    updated test implementation

--- lib/spud_admin.rb
+++ lib/spud_admin.rb
@@ -3,5 +3,6 @@ module Spud
    module Core
    	require 'spud_admin/engine' if defined?(Rails)
    	require 'spud_admin/configuration'
+   	require 'spud_admin/test_files' if ENV["RAILS_ENV"] == 'test'
    end
 end
--- lib/spud_admin/test_files.rb
+++ lib/spud_admin/test_files.rb
@@ -1,22 +1,27 @@
-module SpudAdmin
-  module TestFiles
-    class << self
-      def load_all
-        load_specs
-        load_factories
-        load_support
-      end
-      
-      def load_specs
-        Dir[File.join(File.expand_path('../../../', __FILE__), "spec/**/*_spec.rb")].each {|f| require f}
-      end
+module Spud
+  module Core
+    module TestFiles
+      class << self
+        def initialize
+          load_all
+        end
+        def load_all
+          load_specs
+          load_factories
+          load_support
+        end
         
-      def load_factories
-        Dir[File.join(File.expand_path('../../../', __FILE__), "factories/*")].each {|f| require f}
-      end
-      
-      def load_support
-        Dir[File.join(File.expand_path('../../../', __FILE__), "spec/support/**/*.rb")].each {|f| require f}
+        def load_specs
+          Dir[File.join(File.expand_path('../../../', __FILE__), "spec/**/*_spec.rb")].each {|f| require f}
+        end
+          
+        def load_factories
+          Dir[File.join(File.expand_path('../../../', __FILE__), "factories/*")].each {|f| require f}
+        end
+        
+        def load_support
+          Dir[File.join(File.expand_path('../../../', __FILE__), "spec/support/**/*.rb")].each {|f| require f}
+        end
       end
     end
   end
