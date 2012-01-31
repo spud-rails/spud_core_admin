c478d6992d7db491463c2538b3eee40edf9a4091
Dustin DeYoung <ddeyoung2@gmail.com>
2012-01-30 21:14:44 -0500

    Correct user test.

--- spec/models/spud_user_spec.rb
+++ spec/models/spud_user_spec.rb
@@ -11,12 +11,12 @@ describe SpudUser do
   
     it "should return only the last name if the first name is blank" do
       u = Factory.build(:spud_user, :first_name => nil)
-      u.full_name.should == " #{u.last_name}"
+      u.full_name.should == u.last_name
     end
   
     it "should return only the first name if the last name is blank" do
       u = Factory.build(:spud_user, :last_name => nil)
-      u.full_name.should == "#{u.first_name} "
+      u.full_name.should == u.first_name
     end
   
     it "should return the login if the first and last name are blank" do
