fdad46aed47a218a660e4952f51d055ab8a0b12a
David Estes <destes@redwindsw.com>
2012-03-30 17:51:42 -0400

    adding retina

--- app/assets/javascripts/spud/admin/application.js
+++ app/assets/javascripts/spud/admin/application.js
@@ -56,6 +56,7 @@ $(document).ready(function() {
       // alert('test');
     });
 	  initFormTabs();
+    highdpi_init();
 } );
 
 
@@ -110,4 +111,15 @@ function initDatePicker(selector){
   $(selector).datepicker({
     dateFormat: 'yy-mm-dd'
   }); 
+}
+
+function highdpi_init() {
+  if(jQuery('.replace-2x').css('font-size') == "1px") {
+    var els = jQuery(".replace-2x").get();
+    for(var i = 0; i < els.length; i++) {
+      var src = els[i].src
+      src = src.replace(".png", "@2x.png");
+      els[i].src = src;
+    }
+  }
 }
