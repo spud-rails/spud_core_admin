c10afca06eec32674035968a20d7a45dc428117b
David Estes <destes@redwindsw.com>
2012-04-07 14:21:05 -0400

    fixing utf8 sequencing in tinymce

--- app/controllers/spud/setup_controller.rb
+++ app/controllers/spud/setup_controller.rb
@@ -3,9 +3,9 @@ class Spud::SetupController < Spud::ApplicationController
 	def index
 		@page_thumbnail = "spud/admin/users_thumb.png"
 		@page_name = "First Time Setup"
-		if SpudUser.count != 0
+		if SpudUser.all.count != 0
 				flash[:error] = "Access Denied! This wizard may only be executed when the database is empty."
-				redirect_to root_url() and return
+				redirect_to spud_admin_root_url() and return
 		end
 
 		if request.post?
