5cdb814cec0e7c0c643cdc6c465a1cae510dbb55
David Estes <destes@redwindsw.com>
2012-06-11 10:56:46 -0400

    forcing travis to run

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -5,6 +5,8 @@ require 'rails/all'
 Bundler.require
 require "spud_core"
 
+
+
 module Dummy
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
