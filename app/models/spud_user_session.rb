b640d1d0902bda8e8c3e5db9efc2b90344f6a375
David Estes <destes@redwindsw.com>
2012-08-23 08:48:56 -0400

    generalized authentication errors

--- app/models/spud_user_session.rb
+++ app/models/spud_user_session.rb
@@ -1,3 +1,3 @@
 class SpudUserSession < Authlogic::Session::Base
-  # configuration here, see documentation for sub modules of Authlogic::Session
+  generalize_credentials_error_messages true
 end
--- lib/spud_core/version.rb
+++ lib/spud_core/version.rb
@@ -1,5 +1,5 @@
 module Spud
 	module Core
-	  VERSION = "0.8.22"
+	  VERSION = "0.8.23"
 	end
 end
