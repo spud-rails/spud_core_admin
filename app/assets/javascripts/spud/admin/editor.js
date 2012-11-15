4a1a71577bc561df43ab13b7e968187e0fde8c74
David Estes <destes@redwindsw.com>
2012-11-15 16:12:40 -0500

    updating the valid elements subset

--- app/assets/javascripts/spud/admin/editor.js
+++ app/assets/javascripts/spud/admin/editor.js
@@ -14,6 +14,29 @@ spud.admin.editor = {};
     ['spud_media_picker'],
     []
   ];
+  var validElements = "@[id|class|style|title|dir<ltr?rtl|lang|xml::lang|onclick|ondblclick|" +
+  "onmousedown|onmouseup|onmouseover|onmousemove|onmouseout|onkeypress|" +
+  "onkeydown|onkeyup],a[rel|rev|charset|hreflang|tabindex|accesskey|type|" +
+  "name|href|target|title|class|onfocus|onblur],strong/b,em/i,strike,u," +
+  "#p,-ol[type|compact],-ul[type|compact],-li,br,img[longdesc|usemap|" +
+  "src|border|alt=|title|hspace|vspace|width|height|align|hidpi_src],-sub,-sup," +
+  "-blockquote,-table[border=0|cellspacing|cellpadding|width|frame|rules|" +
+  "height|align|summary|bgcolor|background|bordercolor],-tr[rowspan|width|" +
+  "height|align|valign|bgcolor|background|bordercolor],tbody,thead,tfoot," +
+  "#td[colspan|rowspan|width|height|align|valign|bgcolor|background|bordercolor" +
+  "|scope],#th[colspan|rowspan|width|height|align|valign|scope],caption,-div," +
+  "-span,-code,-pre,address,-h1,-h2,-h3,-h4,-h5,-h6,hr[size|noshade],-font[face" +
+  "|size|color],dd,dl,dt,cite,abbr,acronym,del[datetime|cite],ins[datetime|cite]," +
+  "object[classid|width|height|codebase|*],param[name|value|_value],embed[type|width" +
+  "|height|src|*],script[src|type],map[name],area[shape|coords|href|alt|target],bdo," +
+  "button,col[align|char|charoff|span|valign|width],colgroup[align|char|charoff|span|" +
+  "valign|width],dfn,fieldset,form[action|accept|accept-charset|enctype|method]," +
+  "input[accept|alt|checked|disabled|maxlength|name|readonly|size|src|type|value]," +
+  "kbd,label[for],legend,noscript,optgroup[label|disabled],option[disabled|label|selected|value]," +
+  "q[cite],samp,select[disabled|multiple|name|size],small," +
+  "textarea[cols|rows|disabled|name|readonly],tt,var,big";
+
+
 
   editor.init = function(){
     editor.initWithOptions({});
@@ -33,6 +56,7 @@ spud.admin.editor = {};
       theme_advanced_buttons3: registeredButtons[2].join(','),
       theme_advanced_buttons4: registeredButtons[3].join(','),
       convert_urls: false,
+      valid_elements: validElements,
       height: height
     });
   };
