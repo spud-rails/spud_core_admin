8389bdb883430c2bcbb29f6ab93925005c2daf17
Greg Woods <greg@westlakedesign.com>
2013-02-13 09:26:53 -0500

    Add site name to password reset subject line

--- app/mailers/spud/core_mailer.rb
+++ app/mailers/spud/core_mailer.rb
@@ -5,7 +5,7 @@ class Spud::CoreMailer < ActionMailer::Base
   	@user = user
   	@url = edit_spud_password_reset_url(:id => user.perishable_token)
   	# @url = "/spud/password_resets/#{user.perishable_token}/edit"
-  	  	mail(:from =>Spud::Core.from_address,:to => user.email, :subject => "Forgot Password Request from Spud Site")
+  	  	mail(:from =>Spud::Core.from_address,:to => user.email, :subject => "Forgot Password Request from #{Spud::Core.config.site_name}")
 
   end
 
