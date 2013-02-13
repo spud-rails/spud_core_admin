16f966fbafe196b4dec02344eefcb7ce641e1516
Greg Woods <greg@westlakedesign.com>
2013-02-13 10:04:29 -0500

    move authlogic into a concern so that an application may override it when necessary

---
+++ app/models/concerns/spud_authentication.rb
@@ -0,0 +1,10 @@
+module Concerns
+  module SpudAuthentication
+    extend ActiveSupport::Concern
+    included do
+      acts_as_authentic do |c|
+        # c.my_config_option = my_value # for available options see documentation in: Authlogic::ActsAsAuthentic
+      end # block optional
+    end
+  end
+end
